4906f5ceac5b92e2384f46618f05e12ce2e582b5
lakshyabansal21-debug <lakshyabansal21@gmail.com>
2026-09-21 10:19:34 +0530

    Time: 163 ms (50.89%), Space: 151.1 MB (70.54%) - LeetHub

---
+++ 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<long long> resultArray(vector<int>& nums, int k) {
+        vector<long long> ans(k, 0);
+        vector<long long> dp(k, 0);
+        
+        for (const int num : nums) {
+            vector<long long> newDp(k, 0);
+            const int numMod = num % k;
+            
+            newDp[numMod] = 1;
+            
+            for (int i = 0; i < k; ++i) {
+                if (dp[i] > 0) {
+                    const int newMod = (static_cast<long long>(i) * numMod) % k;
+                    newDp[newMod] += dp[i];
+                }
+            }
+            
+            for (int i = 0; i < k; ++i) {
+                ans[i] += newDp[i];
+            }
+                        
+            
+            dp = std::move(newDp);
+        }
+        
+        return ans;
+    }
+};
